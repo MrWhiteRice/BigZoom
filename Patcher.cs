126e4256229dd4892020a726e6997baf24289bd6
MrWhiteRice <jacob_kotro@hotmail.com>
2023-11-15 22:56:01 +1100

    Initial Push

---
+++ Patcher.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HarmonyLib;
+using UnityEngine;
+
+namespace BigZoom
+{
+	class CameraZoomPatch
+	{
+		[HarmonyPatch(typeof(CameraController), "Awake")]
+		[HarmonyPrefix]
+		static void Prefix(CameraController __instance)
+		{
+			Camera targetCamera = Traverse.Create(__instance).Field("targetCamera").GetValue<Camera>();
+			targetCamera.farClipPlane = 10000f;
+			Traverse.Create(__instance).Field("targetCamera").SetValue(targetCamera);
+
+			Traverse.Create(__instance).Field("maxCameraDist").SetValue(10000f);
+		}
+	}
+
+	class CameraResetPatch
+	{
+		[HarmonyPatch(typeof(CameraController), "Update")]
+		[HarmonyPostfix]
+		static void Postfix(CameraController __instance)
+		{
+			if (UnityEngine.InputSystem.Keyboard.current.rKey.wasPressedThisFrame)
+			{ 
+				__instance.SetDistance(70);
+			}
+		}
+	}
+}
---
+++ Plugin.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace BigZoom
+{
+    [BepInPlugin("Rice.Mods.BigZoom", "Big Zoom", "1.0.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+		public Harmony harmonyMain;
+
+        private void Awake()
+        {
+			harmonyMain = new Harmony(PluginInfo.PLUGIN_GUID);
+			harmonyMain.PatchAll(typeof(CameraZoomPatch));
+			harmonyMain.PatchAll(typeof(CameraResetPatch));
+        }
+    }
+}
